903638d086e0733f098d8b5aa0f3fc2ed55f80d4
Adeeba Ekbal <adeebaekbal@gmail.com>
2025-10-27 12:57:41 +0530

    Add files via upload
    Question9

---
+++ ques9/main9.java
@@ -0,0 +1,11 @@
+package ques9;
+public class main9 
+{
+    public static void main(String [] args)
+    {
+        int gcd = MathOperations.findGCD(2,3);
+        int lcm = MathOperations.findLCM(20,40);
+        System.out.println("GCD:" +gcd);
+        System.out.println("LCM:" +lcm);
+    }
+}
