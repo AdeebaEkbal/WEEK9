ca0a48fff9aa74fed7a53c4cad7688915227f3df
Adeeba Ekbal <adeebaekbal@gmail.com>
2025-10-27 12:43:52 +0530

    Add files via upload
    Question4

--- Account.java
+++ Account.java
@@ -1,26 +1,18 @@
-package ques3;
-public class Account 
+package ques4;
+public abstract class Account 
 {
     int Id;
     String Account_holder_name;
     String Address;
     
-    void Methodsdeposit()
+    abstract void deposit();
+    
+    abstract void withdraw();
+   
+    Account(int i,String name, String add)
     {
-        
-    }
-    void withdraw()
-    {
-        
-    }
-    static void calculateSimpleInterest(int p, int r, int t)
-    {
-        double SI=(p*r*t)/100;                          //int/double
-        System.out.println("Simple interest:" +SI);
-    }                             
-    static void calculateCompoundInterest(int p, int r, int t, int n)
-    {
-        double CI =(p*Math.pow(1+r/t,n*t))-p;           //only double
-        System.out.println("Compound interest:" +CI);
+         Id=i;
+        Account_holder_name=name;
+        Address=add;
     }
 }
---
+++ main4.java
@@ -0,0 +1,8 @@
+package ques4;
+public class main4 
+{
+    public static void main(String [] args)
+    {
+        //can't create objects of abstract class
+    }
+}
