9a74413ebe33f0a5cb4ba30b1a738fbe23655e1b
Adeeba Ekbal <adeebaekbal@gmail.com>
2025-10-27 12:57:05 +0530

    Add files via upload
    Question7

---
+++ ques7/Employee.java
@@ -0,0 +1,8 @@
+package ques7;
+public abstract class Employee 
+{
+    String name;
+    int salary;
+    
+    abstract void showDetails();
+}
---
+++ ques7/main7.java
@@ -0,0 +1,12 @@
+package ques7;
+public class main7 
+{
+    public static void main(String[] args) 
+    {
+        Manager m1=new Manager();
+        m1.name="Iqra";
+        m1.salary=10000;
+        m1.department="HR";
+        m1.showDetails();
+    }
+}
