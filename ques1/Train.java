9262a286ff06de2c2fe66a57237682ade72deed7
Adeeba Ekbal <adeebaekbal@gmail.com>
2025-10-27 12:54:35 +0530

    Add files via upload
    Question1

---
+++ ques1/Train.java
@@ -0,0 +1,10 @@
+package ques1;
+public class Train extends Vehicle
+{
+    int noofcarriages;
+    
+    void display()
+    {
+        System.out.println("Number of carriages:" +noofcarriages);
+    }
+}
---
+++ ques1/Vehicle.java
@@ -0,0 +1,8 @@
+package ques1;
+public class Vehicle 
+{
+    void cost(int cost)
+    {
+        System.out.println("Cost of travelling:" +cost);
+    }
+}
